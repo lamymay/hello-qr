d98ef444fe49132f5879fee2b0f14245a3844ea8
yechao <wb-yc698649@alibaba-inc.com>
2020-07-20 19:33:18 +0800

    inint

---
+++ app/src/main/java/com/arc/hello_qr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.arc.hello_qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
